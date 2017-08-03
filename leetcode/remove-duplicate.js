36fd00f12467b4058a5d522f1efa5999dc688dad
lex alexander <l.alexander10@gmail.com>
2017-08-03 14:13:42 -0700

    Completed remove duplicate.

--- leetcode/remove-duplicate.js
+++ leetcode/remove-duplicate.js
@@ -1,13 +1,19 @@
 var removeDuplicates = function(nums) {
-  var index = 0;
-  for(var i = 0; i < nums.length; i++) {
-    if(nums[i] != nums[i + 1]) {
-      nums[index++] = nums[i]
+  if(nums.length == 0) {
+    return 0
+  }
+  var previous = 0; 
+   for(var next = 1; next < nums.length; next++) {
+    if(nums[next] != nums[previous]) {
+      //if the numbers are unique then 
+      //increment the previous pointer
+      //and move the unique number to 
+      //the duplicate numbers place
+      previous++
+      nums[previous] = nums[next]
     }
   }
-
-  console.log(nums)
+  return previous + 1
 };
 
-
-removeDuplicates([1,1])
+removeDuplicates([1, 1, 2, 2, 3])
