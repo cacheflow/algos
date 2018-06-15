921270ac58d62026be5fa28eb85fdb8b0a117b09
Lex Alexander <l.alexander10@gmail.com>
2018-06-15 16:28:32 -0700

    Pushing wip for valid sudoku.

--- leetcode/valid-sudoku.js
+++ leetcode/valid-sudoku.js
@@ -2,45 +2,51 @@ var isValidSudoku = function(board) {
   let i = 0;
   let j = 0;
   let len = board.length;
-  let rows = []
-  let columns = []
-  let rowSet = new Set()
-  let columnSet = new Set()
-  let isValid = true 
-  let validBoardLen = board.some(b => b.length <= 0)
-  if(validBoardLen) {
+  let rows = {};
+  let columns = {};
+  if(board === null || board.length < 9 || board[0].length < 9) {
      return false
   }
   while(j < len) {
+    //first we check the rows and columns
     let columnVal = board[i][j]
     let rowVal = board[j][i]
     let parsedColumnVal = parseInt(columnVal)
     let parsedRowVal = parseInt(rowVal)
-    if (Number.isInteger(parsedColumnVal)) {
-     if(!columnSet.has(parsedColumnVal)) {
-       columnSet.add(parsedColumnVal)
-     }
-     else {
-       return false 
-     }
-    }
-    if (Number.isInteger(parsedRowVal)) {
-      if (!rowSet.has(parsedRowVal)) {
-        rowSet.add(parsedRowVal)
+    if (Number.isInteger(parsedColumnVal) || Number.isInteger(parsedRowVal)) {
+      if( !(parsedColumnVal in columns) ) {
+        columns[parsedColumnVal] = 1
       }
-      else {
+      if (!(parsedRowVal in columns)) {
+        rows[parsedRowVal] = 1
+      }
+      else if(parsedColumnVal in columns) {
         return false 
       }
+      else if(parsedRowVal in rows) {
+        return false 
+      }
+      else {}
     }
     i++
     if (i === len) {
       j++
       i = 0;
-      columnSet.clear()
-      rowSet.clear()
     }
   }
-  return isValid
+  for (let i = 0; i < 9; i++) {
+    let obj = {}, m1 = Math.floor(i / 3) * 3, m2 = (i % 3) * 3;
+    for (let j = 0; j < 3; j++) {
+      for (let k = 0; k < 3; k++) {
+        let cur = board[m1 + j][m2 + k];
+        console.log("cur is ", cur)
+        if (cur !== '.') {
+          if (obj[cur]) return false;
+          obj[cur] = 1;
+        }
+      }
+    }
+  }
 };
 
 const board = [
